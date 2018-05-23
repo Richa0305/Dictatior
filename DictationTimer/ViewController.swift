3fac60828c4d69209ed29afae6f2862e6cb6a8b8
Srivastava, Richa <richa.srivastava@fmr.com>
2018-05-23 16:40:33 +0530

    Initial Commit

---
+++ DictationTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DictationTimer
+//
+//  Created by Srivastava, Richa on 23/05/18.
+//  Copyright © 2018 Srivastava, Richa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
